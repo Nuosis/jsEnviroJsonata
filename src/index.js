7e9f25e01a2ece367c824c6f06efcc4b18195069
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-04-17 16:39:08 -0700

    initail setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 //Here we're importing items we'll need. You can add other imports here.
 
+import '../node_modules/jsonata/jsonata.min.js'
+
+
 //The first function. Remove this.
 const btn = document.querySelector("button");
 btn.onclick = function () {
