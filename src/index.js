95437f4319f66a3e2acfd722d046869d6dd15aa5
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-04-17 17:07:55 -0700

    js copied from example

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@
 
 import '../node_modules/jsonata/jsonata.min.js'
 
+window.performJSONATA = function (json){
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+  const jsonata = require('jsonata');
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  const query = obj.jquery;
+
+  //call some jsonata magic
+  const outcome = (async () => {
+    const expression = jsonata(query);
+    const result = await expression.evaluate(data);  // returns 24
+    return result
+  });
+  
+  FileMaker.PerformScript("reportResults",JSON.stringify(outcome));
+}
